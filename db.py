d68cea74e8c6639306bbb7116ec42315f23d3164
marshall <marshall.s.lee@gmail.com>
2017-05-13 22:24:34 +0900

    update

--- db.py
+++ db.py
@@ -1,15 +1,20 @@
+import os
 import MySQLdb
 
 class Database:
 
-	host = '/cloudsql/task-manager-marshall:taskmanagergae'
-	db = 'taskmanager'
-	user = 'root'
-	passwd = 'SantaClaus1225^^'
-
 	def __init__(self):
-		self.connection = MySQLdb.connect(self.host, self.db, self.user, self.passwd)
-		self.cursor = self.connection.cursor()
+		self.cloudsql_connection_name = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+		self.cloudsql_user = os.environ.get('CLOUDSQL_USER')
+		self.cloudsql_db = os.environ.get('CLOUDSQL_DB')
+		self.cloudsql_password = os.environ.get('CLOUDSQL_PASSWORD')
+
+		if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+			cloudsql_unix_socket = os.path.join('/cloudsql', self.cloudsql_connection_name)
+			db = MySQLdb.connect(unix_socket=cloudsql_unix_socket, db=self.cloudsql_db, user=self.cloudsql_user, passwd=self.cloudsql_password)
+		else:
+			db = MySQLdb.connect(host='127.0.0.1', db=self.cloudsql_db, user=self.cloudsql_user, password=self.cloudsql_password)
+		return db
 
 	def registerNewUser(self, query):
 		try:
