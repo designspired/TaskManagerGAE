367afecb0d07c3541a1d888f99e6d352ea330ce4
marshall <marshall.s.lee@gmail.com>
2017-05-14 02:39:39 +0900

    update

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ class Register(webapp2.RequestHandler):
 
 	def post(self):
 		name = self.request.POST.get("name")
-		email = self.request.POST.get("email")				
+		email = self.request.POST.get("email")		
+		
 		password = self.request.POST.get("password")
-		uniqueId = uuid.uuid4()
 
 		userdata = {
-			'uuid': uniqueId,
 			'name': name,
 			'email': email,
 			'password': password
 		}
 
+		uniqueId = uuid.uuid4()
 		currentTime = datetime.datetime.now()
 
 		db = Database()
 
-		db.registerNewUser(uuid, name, email, password, currentTime)
+		i = db.registerNewUser(uuid, name, email, password, currentTime)
 
-		jsondata = json.dumps(userdata) 
+		jsondata = json.dumps(i) 
 		
-		self.response.out.write(userdata)
+		self.response.out.write(uniqueId)
 
 class Login(webapp2.RequestHandler):
 	def post(self):
